93cc89b653e022ae9c3b5219e62c8867f0951fff
Heinz N. Gies <heinz@licenser.net>
2016-01-25 13:24:48 -0500

    Revert "Further improvements on zlogin start"
    This reverts commit df15cd49cb06fd83e777ac5e1b75277fcfae9612.

--- apps/fifo_zlogin/src/zlogin_fsm.erl
+++ apps/fifo_zlogin/src/zlogin_fsm.erl
@@ -414,18 +414,10 @@ check_state(UUID) ->
         {ok, Data} ->
             [_, _, State | _] = re:split(Data, ":"),
             case State of
-                <<"running">> ->
-                    check_state1(UUID);
+                <<"running">> ->   running;
                 <<"installed">> -> stopped;
                 _ -> other
             end;
         _ ->
             not_found
-    end.
-
-check_state1(UUID) ->
-    case os:cmd("vmadm get " ++ binary_to_list(UUID) ++
-                    " | json state") of
-        "running\n" -> running;
-        _ -> other
     end.
